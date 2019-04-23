c9d2f2cb283130f3f4c448c3e364ac5a6f86d395
Adrian Sanchez <6586742+AdrianSh@users.noreply.github.com>
2019-04-23 08:56:08 +0100

    Using promises, axios
    https://github.com/axios/axios

--- app/routes/github-langs/Controller.js
+++ app/routes/github-langs/Controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "cx/ui";
+/* import { Controller } from "cx/ui";
 import $ from "jquery";
 import { isContext } from "vm";
 
@@ -14,5 +14,27 @@ export default class extends Controller {
         this.store.set("$page.github.datalang", data);
       }.bind(this)
     );
+  }
+}
+*/
+
+import { Controller } from "cx/ui";
+import axios from "axios";
+import "@babel/polyfill";
+import { isContext } from "vm";
+
+export default class extends Controller {
+  onInit() {
+    this.fetchAllRepos();
+  }
+
+  async fetchAllRepos() {
+    const { data } = await axios.get(
+      "https://api.github.com/search/repositories?q=stars:%3E1+language:" +
+        this.store.get("$route.lang") +
+        "&sort=stars&order=desc&type=Repositories"
+    );
+
+    this.store.set("$page.github.datalang", data);
   }
 }
